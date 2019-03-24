5a7b0cf9efa11608c1b9a7d477db38feeea1fd2a
Jesyfox <jesyfoxbs@gmail.com>
2019-03-24 10:17:40 +0000

    rewrited func views to class views

--- olx_rem/classified_ads/urls.py
+++ olx_rem/classified_ads/urls.py
@@ -5,11 +5,11 @@ from django.contrib.auth.views import LogoutView
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.Index.as_view(), name='index'),
     path('register', views.register, name='login'),
     path('logout', LogoutView.as_view(template_name='index.html', next_page='/'), name='logout')
 ]
 
 urlpatterns += [
-    url(r'^category/(?P<hierarchy>.+)/$', views.show_category, name='category'),
+    url(r'^category/(?P<hierarchy>.+)/$', views.ShowCategory.as_view(), name='category'),
 ]
--- olx_rem/classified_ads/views.py
+++ olx_rem/classified_ads/views.py
@@ -1,48 +1,52 @@
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
-from django.shortcuts import render, get_object_or_404, redirect, reverse, HttpResponseRedirect
-
+from django.shortcuts import render, get_object_or_404, redirect, HttpResponseRedirect
+from django.views.generic import View
 
 from .models import Category, Item
 
 
-def show_category(request, hierarchy=None):
-    category_slug = hierarchy.split('/')
-    parent = None
-    root = Category.objects.all()
-    for slug in category_slug[:-1]:
-        parent = root.get(parent=parent, slug=slug)
+class ShowCategory(View):
+    def get(self, request, hierarchy=None):
+        category_slug = hierarchy.split('/')
+        parent = None
+        root = Category.objects.all()
+        for slug in category_slug[:-1]:
+            parent = root.get(parent=parent, slug=slug)
 
-    try:
-        instance = Category.objects.get(parent=parent, slug=category_slug[-1])
-    except Exception as e:
-        print('Exception!: ', e)
-        instance = get_object_or_404(Item, slug=category_slug[-1])
-        return render(request, 'index.html', {'instance': instance})
-    else:
-        return render(request, 'categories.html', {'instance': instance})
+        try:
+            categories = Category.objects.get(parent=parent, slug=category_slug[-1])
+        except Exception as e:
+            print('Exception!: ', e)
+            categories = get_object_or_404(Item, slug=category_slug[-1])
+            return render(request, 'index.html', {'categories': categories})
+        else:
+            return render(request, 'categories.html', {'categories': categories})
 
 
-def index(request):
-    context = {}
-    instance = Category.objects.all()
-    items = Item.objects.all()
-    auth_form = AuthenticationForm
+class Index(View):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.context = {}
+        self.categories = Category.objects.all()
+        self.items = Item.objects.all()
+        self.auth_form = AuthenticationForm
+        self.context.update(categories=self.categories, items=self.items, form=self.auth_form)
 
-    context.update(instance=instance, items=items, form=auth_form)
-    if request.method == 'POST':
+    def get(self, request):
+        return render(request, 'index.html', context=self.context)
+
+    def post(self, request):
         auth_form = AuthenticationForm(request=request, data=request.POST)
         if auth_form.is_valid():
             username = auth_form.cleaned_data.get('username')
             raw_password = auth_form.cleaned_data.get('password')
             user = authenticate(username=username, password=raw_password)
             login(request, user)
-            return render(request, 'index.html', context=context)
+            return render(request, 'index.html', context=self.context)
         else:
-            context.update(form=auth_form)
-            return render(request, 'index.html', context=context)
-
-    return render(request, 'index.html', context=context)
+            self.context.update(form=auth_form)
+            return render(request, 'index.html', context=self.context)
 
 
 def register(request):
